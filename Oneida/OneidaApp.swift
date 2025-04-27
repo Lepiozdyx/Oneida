a71b41fe183f1f195cbc4b4b149153f197cac7dd
Developer <Lepiozdyx@gmail.com>
2025-04-27 12:05:40 -0300

    Initial Commit

---
+++ Oneida/OneidaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneidaApp.swift
+//  Oneida
+//
+//  Created by Alex on 27.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct OneidaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
